b7bf5b52889ad3009690b792281992a4c6014ba6
Lu1sGabriel <luisgabriel533@gmail.com>
2025-03-14 00:30:07 -0300

    feat: Início do projeto

---
+++ src/main/java/com/luisgoes/ecommerce/ecommerceapi/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luisgoes.ecommerce.ecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luisgoes/ecommerce/ecommerceapi/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgoes.ecommerce.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

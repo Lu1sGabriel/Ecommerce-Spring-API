6800a1397f4876518616873002710f15ee6401d3
Lu1sGabriel <luisgabriel533@gmail.com>
2025-03-14 12:02:29 -0300

    feat: Adição do H2, PostgreSql e JPA. Além da adição do mapamento JPA na entidade `User`

--- src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/User.java
+++ src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/User.java
@@ -1,26 +1,36 @@
 package com.luisgoes.ecommerce.ecommerceapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
+
+@Entity
+@Table(name = "tb_user")
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 @EqualsAndHashCode
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = -4461224141815355852L;
 
+    @Id()
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Setter(AccessLevel.NONE)
     private UUID id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "email")
     private String email;
+    @Column(name = "phone")
     private String phone;
+    @Column(name = "password")
     private String password;
 
 
@@ -31,6 +41,5 @@ public class User implements Serializable {
         this.phone = phone;
         this.password = password;
     }
-
 
 }

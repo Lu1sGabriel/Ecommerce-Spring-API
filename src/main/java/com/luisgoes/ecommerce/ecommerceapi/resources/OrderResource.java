6e83ff785bd09e2d0d1223ba1128cde6aaa607da
Lu1sGabriel <luisgabriel533@gmail.com>
2025-03-15 15:41:27 -0300

    fix: Correção do nome do `@RequestMapping` para manter a nomenclatura padrão estabelecida.

--- src/main/java/com/luisgoes/ecommerce/ecommerceapi/resources/OrderResource.java
+++ src/main/java/com/luisgoes/ecommerce/ecommerceapi/resources/OrderResource.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/api/order")
+@RequestMapping(value = "/api/orders")
 public class OrderResource {
 
     @Autowired

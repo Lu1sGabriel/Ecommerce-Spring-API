12d43dba168991bacf6d487af1da5899460139b9
Lu1sGabriel <luisgabriel533@gmail.com>
2025-03-17 21:09:23 -0300

    feat: Adição da anotação `@Setter(AccessLevel.NONE)` na entidade `Product`

--- src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/Product.java
+++ src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/Product.java
@@ -44,6 +44,7 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @Setter(AccessLevel.NONE)
     private Set<Category> categories = new HashSet<>();
 
 

ba62843355e202ec19fda05273a90114297f7eeb
Lu1sGabriel <luisgabriel533@gmail.com>
2025-03-25 13:35:10 -0300

    feat: Adição da anotação ´@EqualsAndHashCode` em OrderItemPk

--- src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/primaryKeys/OrderItemPK.java
+++ src/main/java/com/luisgoes/ecommerce/ecommerceapi/entities/primaryKeys/OrderItemPK.java
@@ -6,6 +6,7 @@ import com.luisgoes.ecommerce.ecommerceapi.entities.Product;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +16,7 @@ import java.io.Serializable;
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class OrderItemPK implements Serializable {
 
     @Serial
